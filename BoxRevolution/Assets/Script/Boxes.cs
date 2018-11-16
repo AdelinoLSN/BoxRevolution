65c6f7ccb2c3866efb760bd007aa1b03a1acab19
Mateus <mateus.resende.r@gmail.com>
2018-11-16 16:33:18 -0300

    Mundo

---
+++ BoxRevolution/Assets/Script/Boxes.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading;
+
+public class Boxes : MonoBehaviour {
+    public GameObject box;
+    public Transform teto;
+
+    
+
+    // Use this for initialization
+    void Start () {
+        Instantiate(box,teto.position,teto.rotation);
+    }
+
+    
+    // Update is called once per frame
+    void Update () {
+        
+    }
+}
