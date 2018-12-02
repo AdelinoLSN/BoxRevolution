e2a645b781b7c00d1a87114b8313b728f8db61bb
EbersonSantos <eberson.santos1@gmail.com>
2018-12-02 00:30:45 -0300

    adicionado camera que segue o personagem

---
+++ BoxRevolution/Assets/Script/CameraController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject target;
+    public GameObject[] positions;
+    private int index = 0;
+    public float speed = 2;
+    private RaycastHit hit;
+
+    void FixedUpdate()
+    {
+        transform.LookAt(target.transform);
+
+        //check for collision
+        if (!Physics.Linecast(target.transform.position, positions[index].transform.position))
+        {
+            transform.position = Vector3.Lerp(transform.position, positions[index].transform.position, speed*Time.deltaTime);
+        }else if (Physics.Linecast(target.transform.position, positions[index].transform.position, out hit))
+        {
+            transform.position = Vector3.Lerp(transform.position, hit.point, speed * 2 * Time.deltaTime);
+             
+        }
+    }
+
+}
