58af4b00996a9436e698464f27f16b5bd7df0cfd
EbersonSantos <eberson.santos1@gmail.com>
2018-12-20 04:50:37 -0300

    cai uma caixa aleatroia e depois outra no jogador

--- BoxRevolution/Assets/Script/Boxes.cs
+++ BoxRevolution/Assets/Script/Boxes.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using UnityEngine;
+using System.Collections;
 
 public class Boxes : MonoBehaviour
 {
@@ -11,17 +12,33 @@ public class Boxes : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-        InvokeRepeating("Spawn", 6f, 6f);
-        
+        InvokeRepeating("Spawn", 2f, 20f);
+
     }
 
     void Spawn()
     {
+        StartCoroutine(CreateBoxes());
+    }
+
+    IEnumerator CreateBoxes()
+    {
+        Box1();
+        yield return new WaitForSeconds(10);
+        Box2();
+    }
+
+    void Box1()
+    {
         int x = random.Next(-7, 7);
         int z = random.Next(-7, 7);
         Instantiate(box, new Vector3(x, 15, z), teto.rotation);
+    }
+
+    void Box2()
+    {
         Instantiate(box, new Vector3(alvo.transform.position.x, 15, alvo.transform.position.z), teto.rotation);
     }
 
-   
+
 }
