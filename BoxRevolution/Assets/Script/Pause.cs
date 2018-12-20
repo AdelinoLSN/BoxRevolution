98f4685e9310da3ab93d67e9cfea5a19614be4f5
Djeymisson <djeymisson@gmail.com>
2018-12-20 00:17:54 -0300

    Adicionado menu de pausa
    Quando o usuario pressina ESC, o jogo pausa e aparece um menu com as opçãoes de voltar ao jogo e sair dele.

---
+++ BoxRevolution/Assets/Script/Pause.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pause : MonoBehaviour {
+
+    public GameObject pause_panel;
+
+    void Start()
+    {
+        OnUnPause();   
+    }
+
+    void Update()
+    {
+        bool pause = Input.GetButtonDown("Cancel");
+
+        if (pause)
+        {
+            OnPause();
+        }
+    }
+
+    public void OnPause()
+    {
+        pause_panel.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void OnUnPause()
+    {
+        pause_panel.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public void goToMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+}
